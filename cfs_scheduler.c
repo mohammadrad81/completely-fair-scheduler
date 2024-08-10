c29df42cc88fa1794eeda8f21109bc54ec9d0b27
mohammadrad81 <mohammad.rad81@gmail.com>
2024-08-10 23:23:58 +0330

    prepared for the lab

--- cfs_scheduler.c
+++ cfs_scheduler.c
@@ -8,59 +8,37 @@
 
 
 void fill_process_array(process* process_array[PROCESS_COUNT]){
-    for(int i = 0; i < PROCESS_COUNT; i++){
-        process_array[i] = create_process(2000 + i, // process id
-                                          100 + i,  // current vruntime
-                                          3);       // residual execution duration of the process
-    }
+    // fill the process_array with processes
 }
 
 void insert_one_process_to_rbtree(rbtree *rbt, process* proc){
-    mydata *data = makedata_with_object(proc->vruntime, proc);
-    if(rb_insert(rbt, data) == NULL){
-        fprintf(stderr, "insert %d: out of memory\n", proc->id);
-    }
+    // make mydata instance of process and insert it to the rbtree, the key is proc->vruntime
 }
 
 void insert_processes_to_rbtree(rbtree *rbt, process *process_array[PROCESS_COUNT]){
-    process *proc;
-    for(int i = 0; i < PROCESS_COUNT; i++){
-        proc = process_array[i];
-        insert_one_process_to_rbtree(rbt, proc);
-    }
+    // insert processes in process_array to rbtree
 }
 
 process* process_of_node(rbnode* node){
-    process* proc = (process *)((mydata *)(node->data))->object;
-    return proc;
+    // extract the process in node
 }
 
 int main(){
-    process* processes[PROCESS_COUNT];
-    fill_process_array(processes);
+    // create an array of procdesses and fill it
     printf("process array filled\n");
-    rbtree *rbt;
-    if((rbt = rb_create(compare_func, destroy_func)) == NULL){
-        fprintf(stderr, "create red-black tree failed\n");
-    }
-    else{
-        printf("tree created\n");
-    }
-
-    insert_processes_to_rbtree(rbt, processes);
+    // create a red-black tree
+    
+    // insert processes to rbtree
     printf("inserted processes to rbtree\n");
 
-    rbnode *node;
-    process* current_proc;
-    int current_tick = 0;
-    while((node = RB_MINIMAL(rbt))){
-        printf("current_tick: %d\n", current_tick);
-        current_tick ++;
-        current_proc = process_of_node(node);
-        run_process_for_one_tick(current_proc);
-        rb_delete(rbt, node, 0);
-        if(!is_terminated(current_proc)){
-            insert_one_process_to_rbtree(rbt, current_proc);
-        }
+    // declare node, current_proc and current_tick
+
+    //scheduling algorithm:
+    while(
+        // condition of loop
+        ){
+        // execute the process in left-most node of rbtree
+        // increment current_tick
+        // if process still has positive residual_duration, reinsert it to the rbtree 
     }
 }
